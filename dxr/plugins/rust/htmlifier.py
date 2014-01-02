2ba1d6ea5678fef8a988da808ddb1d68fbfd48bd
Nick Cameron <ncameron@mozilla.com>
2014-01-02 11:38:20 +0000

    intermediate commit - fns and vars, multiple csv files (in progress)

--- dxr/plugins/rust/htmlifier.py
+++ dxr/plugins/rust/htmlifier.py
@@ -1,11 +1,153 @@
+import os
+import fnmatch
 import dxr.plugins
+from dxr.utils import search_url
 
+_tree = None
+_conn = None
 
 def load(tree, conn):
-  pass
+    global _tree, _conn
+    _tree = tree
+    _conn = conn
 
+class RustHtmlifier(object):
+    def __init__(self, tree, conn, path, text, file_id):
+        self.tree    = tree
+        self.conn    = conn
+        self.path    = path
+        self.text    = text
+        self.file_id = file_id
+
+    def regions(self):
+        return []
+
+    def refs(self):
+        """ Generate reference menus """
+        # We'll need this argument for all queries here
+        args = (self.file_id,)
+
+        # Extents for functions definitions
+        sql = """
+            SELECT extent_start, extent_end, qualname
+                FROM functions
+              WHERE file_id = ?
+        """
+        for start, end, qualname in self.conn.execute(sql, args):
+            yield start, end, (self.function_menu(qualname), qualname, None)
+
+        # Add references to functions
+        sql = """
+            SELECT refs.extent_start, refs.extent_end,
+                          functions.qualname,
+                          (SELECT path FROM files WHERE files.id = functions.file_id),
+                          functions.file_line
+                FROM functions, function_refs AS refs
+              WHERE functions.id = refs.refid AND refs.file_id = ?
+        """
+        for start, end, qualname, path, line in self.conn.execute(sql, args):
+            menu = self.function_menu(qualname)
+            self.add_jump_definition(menu, path, line)
+            yield start, end, (menu, qualname, None)
+
+        # Extents for variables defined here
+        sql = """
+            SELECT extent_start, extent_end, qualname, value
+                FROM variables
+              WHERE file_id = ?
+        """
+        for start, end, qualname, value in self.conn.execute(sql, args):
+            yield start, end, (self.variable_menu(qualname), qualname, value)
+
+        # Add references to variables
+        sql = """
+            SELECT refs.extent_start, refs.extent_end,
+                          variables.qualname,
+                          variables.value,
+                          (SELECT path FROM files WHERE files.id = variables.file_id),
+                          variables.file_line
+                FROM variables, variable_refs AS refs
+              WHERE variables.id = refs.refid AND refs.file_id = ?
+        """
+        for start, end, qualname, value, path, line in self.conn.execute(sql, args):
+            menu = self.variable_menu(qualname)
+            self.add_jump_definition(menu, path, line)
+            yield start, end, (menu, qualname, value)
+
+
+    def search(self, query):
+        """ Auxiliary function for getting the search url for query """
+        return search_url(self.tree.config.wwwroot,
+                          self.tree.name,
+                          query)
+
+    def quote(self, qualname):
+        """ Wrap qualname in quotes if it contains spaces """
+        if ' ' in qualname:
+            qualname = '"' + qualname + '"'
+        return qualname
+
+    def function_menu(self, qualname):
+        """ Build menu for a function """
+        menu = []
+        menu.append({
+            'text':   "Find references",
+            'title':  "Find references to this function",
+            'href':   self.search("+function-ref:%s" % self.quote(qualname)),
+            'icon':   'reference'
+        })
+        return menu
+
+    def variable_menu(self, qualname):
+        """ Build menu for a variable """
+        menu = []
+        menu.append({
+            'text':   "Find references",
+            'title':  "Find reference to this variable",
+            'href':   self.search("+var-ref:%s" % self.quote(qualname)),
+            'icon':   'field'
+        })
+        return menu
+
+    def add_jump_definition(self, menu, path, line):
+        """ Add a jump to definition to the menu """
+        # Definition url
+        url = self.tree.config.wwwroot + '/' + self.tree.name + '/source/' + path
+        url += "#%s" % line
+        menu.insert(0, { 
+            'text':   "Jump to definition",
+            'title':  "Jump to the definition in '%s'" % os.path.basename(path),
+            'href':   url,
+            'icon':   'jump'
+        })
+
+    def annotations(self):
+        # TODO - compiler warnings
+        return []
+
+    def links(self):
+        # TODO when we have methods in the functions table, we will need to be more
+        # selective here
+        # TODO only want top level functions
+        # TODO probably need to think about how to organise the whole side bar thing properly
+        sql = "SELECT name, file_line FROM functions WHERE file_id = ? ORDER BY file_line"
+        links = []
+        for name, line in self.conn.execute(sql, (self.file_id,)):
+            if len(name) == 0: continue
+            links.append(('function', name, "#%s" % line))
+        yield (30, 'functions', links)
+
+# XXX Does anyone use anything other than rs nowadays?
+_patterns = ('*.rs', '*.rc')
 def htmlify(path, text):
-  print("Rust htmlifier called")
-  pass
+    fname = os.path.basename(path)
+    if any((fnmatch.fnmatchcase(fname, p) for p in _patterns)):
+        # Get file_id, skip if not in database
+        sql = "SELECT files.id FROM files WHERE path = ? LIMIT 1"
+        row = _conn.execute(sql, (path,)).fetchone()
+        if row:
+            print "Rust htmlification - " + path
+            return RustHtmlifier(_tree, _conn, path, text, row[0])
+    return None
 
 __all__ = dxr.plugins.htmlifier_exports()
