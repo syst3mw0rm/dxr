6e4fd331fa5d9fce2a5d974ec9a0c3ba266ef3bb
Nick Cameron <ncameron@mozilla.com>
2014-02-05 23:34:01 +1300

    rebase to rust master

--- tests/test_rust/code/SubDir/mod.rs
+++ tests/test_rust/code/SubDir/mod.rs
@@ -2,15 +2,18 @@
 
 use msalias = sub::sub2;
 use sub::sub2;
+use std::io::stdio::println;
 
 static yy: uint = 25u;
 
 mod sub {
     pub mod sub2 {
+        use std::io::stdio::println;
         pub mod sub3 {
-          pub fn hello() {
-              println("hello from module 3");
-          }          
+            use std::io::stdio::println;
+            pub fn hello() {
+                println("hello from module 3");
+            }          
         }
         pub fn hello() {
             println("hello from a module");
